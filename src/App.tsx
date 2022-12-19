d87dfc0d368b167997d11ccc51a5f6c64415bf79
Victor Owiti <viowiti12@gmail.com>
2022-12-19 10:47:23 +0300

    Completed navbar markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="max-w-[1440px] m-auto">
+      <BrowserRouter>
+        <NavBar />
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
