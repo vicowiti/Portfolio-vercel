9f99115e66875f4cbce396f5acbc9affb09a9f5d
Victor Owiti <viowiti12@gmail.com>
2022-12-20 12:01:10 +0300

    Added the hero page

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLottie } from "lottie-react";
 import DevAnimation from "../assets/lottie.json";
-import Typewriter from "typewriter-effect/dist";
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
   const options = {
@@ -17,12 +17,21 @@ const Banner = () => {
           <div className="w-[300px] lg:w-full">{View}</div>
           <div className="w-[300px] lg:w-full flex flex-col justify-center items-center">
             <h1 className="text-secondary lg:text-5xl font-bold">
-              Box Office News!
+              <Typewriter
+                options={{
+                  strings: ["Hello, I am Victor", "A Software Developer"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
             </h1>
-            <p className="py-6 lg:text-lg text-sm text-center ">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <p className="py-6 lg:text-lg text-sm text-left ">
+              I am a web developer with a strong background in React. As a
+              developer, I have a passion for creating intuitive and engaging
+              user experiences through clean, efficient code. My experience with
+              React has allowed me to build dynamic and responsive websites and
+              applications that can handle large amounts. I am always looking
+              for new challenges and opportunities to grow as a developer.
             </p>
             {/* <button className="btn btn-primary">Get Started</button> */}
           </div>
