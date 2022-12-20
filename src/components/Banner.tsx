a88d38ab8532df004e385fa0a792ee02b1d5a673
Victor Owiti <viowiti12@gmail.com>
2022-12-20 10:45:08 +0300

    Added lottie animation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,24 +1,30 @@
 import React from "react";
+import { useLottie } from "lottie-react";
+import DevAnimation from "../assets/lottie.json";
+import Typewriter from "typewriter-effect/dist";
 
 const Banner = () => {
+  const options = {
+    animationData: DevAnimation,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
   return (
     <div>
-      <div className="bg-base-200">
+      <div className="bg-base-200 p-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5">
-          <div>
-            <img
-              src="https://placeimg.com/260/400/arch"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
-          </div>
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
+          <div className="w-[300px] lg:w-full">{View}</div>
+          <div className="w-[300px] lg:w-full flex flex-col justify-center items-center">
+            <h1 className="text-secondary lg:text-5xl font-bold">
+              Box Office News!
+            </h1>
+            <p className="py-6 lg:text-lg text-sm text-center ">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            {/* <button className="btn btn-primary">Get Started</button> */}
           </div>
         </div>
       </div>
